db7662f6019959340a6fb0f4a1cfb74c56f24b63
delucia <oguzhankukul@gmail.com>
2023-04-07 17:57:26 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   const month = useRef();
   const year = useRef();
 
-  const [years, setYears] = useState();
-  const [months, setMonths] = useState();
-  const [days, setDays] = useState();
+  const [years, setYears] = useState('--');
+  const [months, setMonths] = useState('--');
+  const [days, setDays] = useState('--');
   
   const calculate = () => {
 
@@ -27,17 +27,40 @@ function App() {
   }
 
   return (
-    <div>
-      <p>Day</p>
-      <input type='text' ref={day} maxLength='2' placeholder="DD" required></input>
-      <input type='text' ref={month} maxLength='2' placeholder="MM" required></input>
-      <input type='text' ref={year} maxLength='4' placeholder="YYYY" required></input>
-      
-      <br></br>
-      <button onClick={calculate}>Calculate</button>
-      <h3>{years}years</h3>
-      <h3>{months}months</h3>
-      <h3>{days}days</h3>
+    <div className='container'>
+      <div className='box'>
+        <div className='input-container'>
+          <div className='input-cell'>
+            <label name='day'>D A Y</label>
+            <input className='input' type='text' ref={day} maxLength='2' placeholder="DD" required></input>
+          </div>
+          <div className='input-cell'>
+            <label name='month'>M O N T H</label>
+            <input className='input' type='text' ref={month} maxLength='2' placeholder="MM" required></input>
+          </div>
+          <div className='input-cell'>
+            <label name='year'>Y E A R</label>
+            <input className='input' type='text' ref={year} maxLength='4' placeholder="YYYY" required></input>
+          </div>
+        </div>
+
+        <div className='divider'>
+          <hr></hr>
+          <button onClick={calculate}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44">
+              <g fill="none" stroke="#FFF" strokeWidth="2">
+                <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/>
+              </g>
+            </svg>
+          </button>
+        </div>
+        
+        <div className='results-container'>
+          <h3><span className='result'>{years}</span>years</h3>
+          <h3><span className='result'>{months}</span>months</h3>
+          <h3><span className='result'>{days}</span>days</h3>
+        </div>
+      </div>
     </div>
   );
 }
